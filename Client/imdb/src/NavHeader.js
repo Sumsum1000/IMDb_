bcbff2a7b9cced8a150cde0c7ade76f6d8b776ba
reut <justreut1@gmail.com>
2021-08-17 17:02:10 +0300

    navbar

--- Client/imdb/src/NavHeader.js
+++ Client/imdb/src/NavHeader.js
@@ -19,13 +19,20 @@ export default function NavHeader() {
             </div>
             <div className='search-area'>
                 <div className='rect-search-container'>
-                    <div className='left-search'>
-                        <span>All</span>
-                        <span className='arrow'></span>
-                        <div className='search-devider'></div>
-                        <span>Search IMDb</span>
-                    </div>
-                    <img className='search-icon' src={serachIcon} />
+                    <form class="nav-search-form"  method="get" action="/find">
+                        <div class="search-category-selector">
+                            <label class="button" tabindex="0">
+                                <div class="button-text">All<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="black" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M8.71 11.71l2.59 2.59c.39.39 1.02.39 1.41 0l2.59-2.59c.63-.63.18-1.71-.71-1.71H9.41c-.89 0-1.33 1.08-.7 1.71z"></path></svg>
+                                </div>
+                            </label>
+                        </div>
+                        <div class="searchTyping">
+                            <input class="imdb-header-search-input"  type="text" value="" autocomplete="off" name="q" placeholder="Search IMDb" autocapitalize="off" autocorrect="off"/>
+                        </div>
+                        <button class="suggestion-search-button" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#828282" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
+                        </button>
+                    </form>
                 </div>
             </div>
             <div className="IMDbPro">
