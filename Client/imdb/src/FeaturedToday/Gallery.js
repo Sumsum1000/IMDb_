645a7c2db60895dd57b95b44493b18fc2008e20d
Asaf Levi <asafl14@yahoo.com>
2021-09-22 23:42:27 +0300

    endless scroll before css

--- Client/imdb/src/FeaturedToday/Gallery.js
+++ Client/imdb/src/FeaturedToday/Gallery.js
@@ -1,4 +1,5 @@
 import './Gallery.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { useContext } from "react";
 import { MoviesContext } from "../MoviesContext";
 import { UserContext } from '../User.context';
@@ -7,20 +8,35 @@ import star from '../Images/YellowStar.png';
 
 export default function Gallery(isColored) {
 
+    const {featured, fetchFeatured, fetchData, hasMore, sethasMore,
+           page, setPage} = useContext(MoviesContext);
 
-    const {featured} = useContext(MoviesContext);
 
     return(
+        <InfiniteScroll
+             dataLength={featured.length}
+             next={fetchData}
+             hasMore={true}
+             loader={<h4>Loading...</h4>}
+             endMessage={
+                 <p style={{ textAlign: 'center' }}>
+                 <b>Yay! You have seen it all</b>
+                 </p>
+            }
+        >
+                <div className='gallery'>
+                    {featured.map(movie => <MovieCard  starSrc={star}
+                    vote_average={movie.vote_average}
+                    original_title={movie.original_title}
+                    movieId={`/MovieToday/${movie.id}`}
+                    imgUrl={
+                        `https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                    />)}
+                </div>
+        </InfiniteScroll>
+                
 
-        <div className='gallery'>
-            {featured.map(movie => <MovieCard  starSrc={star}
-            vote_average={movie.vote_average}
-            original_title={movie.original_title}
-            movieId={`/MovieToday/${movie.id}`}
-            imgUrl={
-                 `https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-            />)}
-        </div>
+ 
     )
 }
 
--- Client/imdb/src/MoviesContext.js
+++ Client/imdb/src/MoviesContext.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export const MoviesContext = React.createContext({
   movies: []
@@ -7,6 +8,8 @@ export const MoviesContext = React.createContext({
 export default function MoviesProvider({ children }) {
   const [movies, setMovies] = useState([]);
   const [featured, setFeatured] = useState([]);
+  const [hasMore, sethasMore] = useState(true);
+  const [page, setPage] = useState(2);
   
   useEffect(() => {
     fetch('/api/Movies')
@@ -14,17 +17,41 @@ export default function MoviesProvider({ children }) {
       .then(data => setMovies(data));
 
 
-      fetch('/api/Movies/featured')
+      fetch('/api/Movies/featured?_page=${page}&_limit=20')
       .then(response => response.json())
       .then(data => setFeatured(data));
 
   }, []);
 
+  const fetchFeatured = async () => {
+    const res = await fetch(
+      '/api/Movies/featured?_page=${page}&_limit=20'
+    );
+    const data = await res.json();
+    return data;
+  };
+
+  const fetchData = async () => {
+    const featuredFormServer = await fetchFeatured();
+
+    setFeatured([...featured, ...featuredFormServer]);
+    if (featuredFormServer.length === 0 || featuredFormServer.length < 20) {
+      sethasMore(false);
+    }
+    setPage(page + 1);
+  };
+
   return (
     <MoviesContext.Provider value={{
       movies,
       setMovies,
-      featured
+      featured,
+      fetchFeatured,
+      fetchData,
+      hasMore,
+      sethasMore,
+      page,
+      setPage
     }}>
       {children}
     </MoviesContext.Provider>
